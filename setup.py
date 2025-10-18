1ef1eccecf2d9fc30625cc6c1ca5c59fef8689ee
Dexter <dexterong94@gmail.com>
2025-10-18 08:41:14 -0400

    Fixed setup

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'README.md']),
         ('share/' + package_name + '/launch', ['launch/waypoint_manager.launch.py']),
     ],
     install_requires=['setuptools', 'PyQt6'],
